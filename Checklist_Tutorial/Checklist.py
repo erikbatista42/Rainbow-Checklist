dfe1b0f301302fb9d59904592f55c64f7995933e
ErikBatista <erikbatista42@gmail.com>
2018-08-28 12:07:56 -0700

    Checklist working except for read function

--- Checklist_Tutorial/Checklist.py
+++ Checklist_Tutorial/Checklist.py
@@ -17,6 +17,12 @@ def update(index, item): # update item code here
 def destroy(index): # destory item code here
     checklist.pop()
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
+
 def mark_completed(index):
     # checklist.insert() = index
     #  complete_Mark = checklist.insert(index, "√")
@@ -27,28 +33,58 @@ def mark_completed(index):
     #  complete_Mark = checklist[index]  + "√"
     return complete_Mark
 
-def list_all_items():
-    index = 0
-    for list_item in checklist:
-        print("{} {}".format(index, list_item))
-        index += 1
+def user_input(prompt):
+    user_input = input(prompt)
+
+    return user_input
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = input("Input item:")
+        create(input_item)
+    #read item
+    elif function_code == "R":
+        item_index = input("Index Number?")
+        # item_index must exist else our program will crash
+        read(item_index)
+
+        # print all items
+    elif function_code == "P":
+            list_all_items()
+        # catch all
+    elif function_code == "Q":
+        return False
+    else:
+        print("Unknown Option")
+    return True
 
 def test(): # test the functions above
-    create("purple sox") # Creates elements in the checklist
-    create("red cloak")
+    # create("purple sox") # Creates elements in the checklist
+    # create("red cloak")
     # print(read(0))
     # print(read(1))
     # update(0, "purple socks")
     # destroy(1)
     # print(read(0))
-    # # print(read(1))
+    # print(read(1))
 
     # print(checklist)
     # list_all_items()
     # create("bananas")
-    print(mark_completed(0))
-    print(checklist)
+    # print(mark_completed(0))
+    running = True
+    while running:
+        selection = user_input("Press C to add to list, R to read from list, P to display list and Q to quit")
+        running = select(selection)
 
+    # print(checklist)
+    # select("C")
+    # list_all_items()
+    # select("R")
+    list_all_items()
+    user_value = input("Please enter a value:")
+    print(user_value)
+
+    
 test() # runs the above function
-
-
