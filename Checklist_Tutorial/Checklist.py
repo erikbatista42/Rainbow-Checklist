5ae90d8cc7fc990b479295b86e7d8fd432746d5b
ErikBatista <erikbatista42@gmail.com>
2018-08-27 19:45:12 -0700

    more code

--- Checklist_Tutorial/Checklist.py
+++ Checklist_Tutorial/Checklist.py
@@ -1,19 +1,32 @@
-# creating a list
-checklist = list()
+# # creating a list
+# checklist = list()
+# print(checklist)
+
+# checklist.append("hello")
+# print(checklist)
+
+# checklist.append("world")
+# print(checklist)
+
+# # a function that adds something to our checklist
+# def add_something_to_my_checklist(add_item):
+#     checklist.append(add_item)
+# # adding something to our list when our function is called
+# add_something_to_my_checklist("i'm")
+# print(checklist)
+
+# add_something_to_my_checklist("erik")
+# print(checklist)
+
+# def read(index):
+#     # item = checklist[index]
+#     print(index)
+#     return checklist[index]
+# read(checklist[3])
+
+checklist = ["Hello", "world"]
+print(checklist)
+checklist[1] = "cats"
 print(checklist)
 
-checklist.append("hello")
-print(checklist)
-
-checklist.append("world")
-print(checklist)
-
-# a function that adds something to our checklist
-def add_something_to_my_checklist(add_item):
-    checklist.append(add_item)
-# adding something to our list when our function is called
-add_something_to_my_checklist("i'm")
-print(checklist)
-
-add_something_to_my_checklist("erik")
-print(checklist)
+# def update
