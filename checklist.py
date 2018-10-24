a8c18af9fbd238ebcc28f8c2a0cc726106d293d5
luxury <erikbatista42@gmail.com>
2018-10-23 19:01:32 -0700

    update

--- checklist.py
+++ checklist.py
@@ -1,43 +1,71 @@
-# CRUD -- create, read, update, and destroy
-
 checklist = list()
 
-# Create
 def create(item):
+    # Create item code here
     checklist.append(item)
 
-# Read
+def read(index):
+    # Read code here
     return checklist[index]
-# Update
+
 def update(index, item):
+    # Update code here
     checklist[index] = item
 
-# Destroy
 def destroy(index):
+    # Destroy code here
     checklist.pop(index)
 
-def lists_all_items():
+def list_all_items():
+    # Code to list all items in list
     index = 0
     for list_item in checklist:
-        print(index + list_item)
+        print(str(index) + list_item)
         index += 1
 
-# Test
+def mark_completed(index):
+    # Complete the function above. Hint: Add a character to the front of the checklist item that denotes an item as completed.
+    # All we need to do is append some text to the front of the checked item in the list. Let's use the character √ to indicate whether an item is marked as completed or not.d
+    pass
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = input("Input item:")
+        create(input_item)
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index number?")
+
+        # Remember that item_index must actually exist our porgram will crash
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    ## This is where we want to stop our loop
+    elif function_code == "Q":
+        return False
+
+    # Catch all
+    else:
+        print("Unknown Option")
+    return True
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+
+
 def test():
-    create("purple sox")
-    create("red cloak")
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-
-    destroy(1)
-
-    print(read(0))
-
-    list_all_items()
-
-
+    running = True
+    while running:
+        selection = user_input("Press C to add to list, R to Read from list and P to display list")
+        select(selection)
 
 
 test()
